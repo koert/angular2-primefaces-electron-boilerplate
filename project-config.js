28e0c8e41190598dd07e3b1f5f9df708380c576b
Koert Zeilstra <koert@zencode.nl>
2016-12-03 12:25:43 +0100

    Setup

---
+++ project-config.js
@@ -0,0 +1,46 @@
+exports.VENDOR_FILES = [
+    {
+        source: "reflect-metadata",
+        files: "Reflect.js"
+    },
+    {
+        source: "systemjs",
+        files: "dist/system.src.js"
+    },
+    {
+        source: "zone.js",
+        files: "dist/zone.js"
+    },
+    {
+        source: "@angular",
+        files: "**/*"
+    },
+    {
+        source: "rxjs",
+        files: "**/*"
+    },
+    {
+        source: "core-js",
+        files: "**/*"
+    },
+    {
+        source: "primeng",
+        files: "**/*"
+    },
+    {
+        source: "font-awesome",
+        files: "**/*"
+
+    }
+]
+
+exports.BOWER_PACKAGES = [
+    {
+        source: "bootstrap",
+        files: "dist/**/*"
+    },
+    {
+        source: "web-animations-js",
+        files: "web-animations.min.js"
+    }
+]
