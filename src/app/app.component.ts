9b239d6c9182aeb1aec27f72e7b8d868fe13895c
Koert Zeilstra <koert@zencode.nl>
2016-12-03 12:01:39 +0100

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MdToolbar } from '@angular/material';
-import {MdSnackBar, MdSnackBarConfig} from '@angular/material';
-import {MdLiveAnnouncer} from '@angular/material';
 
 const ipcRenderer = require('electron').ipcRenderer;
 
@@ -12,11 +9,12 @@ const ipcRenderer = require('electron').ipcRenderer;
 })
 export class AppComponent implements OnInit {
 
-  constructor(private snackBar: MdSnackBar, private live: MdLiveAnnouncer) {
+  constructor() {
   }
 
   public title: string = "Hello Angular 2!";
-  public message: string = "Greatness awaits..."
+  public message: string = "Greatness awaits...";
+  messages: any[];
   public imagePath: string;
 
   ngOnInit(): void {
@@ -34,14 +32,11 @@ export class AppComponent implements OnInit {
     console.log("Getestet");
     ipcRenderer.send("message", "tested");
 
-    let config = new MdSnackBarConfig();
-    // config.duration = 0;
-    let snackBar = this.snackBar.open(this.message, null, config);
-    setTimeout(() => { snackBar.dismiss(); }, 5000);
+    this.messages.push({severity: 'info', summary: 'Test', detail: this.message});
   }
 
   public announce(): void {
-    this.live.announce(this.message);
+    this.messages.push({severity: 'info', summary: 'Test', detail: this.message});
   }
 
   public openImageFileSelect(): void {
@@ -50,8 +45,7 @@ export class AppComponent implements OnInit {
 
   private selectImageFile(path: string): void {
     this.imagePath = path;
-    let snackBarRef = this.snackBar.open(`Selected path: ${path}`, null, new MdSnackBarConfig());
-    setTimeout(() => snackBarRef.dismiss(), 5000);
+    this.messages.push({severity: 'info', summary: 'File selected', detail: `Selected path: ${path}`});
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
-import { MaterialModule } from '@angular/material';
+
+import {ButtonModule} from 'primeng/primeng';
+import {GrowlModule} from 'primeng/primeng';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MaterialModule.forRoot()],
+  imports: [BrowserModule, FormsModule,
+    ButtonModule, GrowlModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  messages: any[] = [];
+
+}
