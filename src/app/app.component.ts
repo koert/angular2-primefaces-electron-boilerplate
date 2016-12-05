b9a0bb74f419f90d7e9393e423c52ce4ea611cb4
Koert Zeilstra <koert@zencode.nl>
2016-12-05 19:48:17 +0100

    Added styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ const ipcRenderer = require('electron').ipcRenderer;
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent implements OnInit {
 
